49478fbbcb26b156b2ec2e5e121fdae4bb703675
Robert Wallhead <rwallhead@gmail.com>
2012-04-23 19:07:08 +1000

    Horizontal parsing in steps, showing result

--- runtests.py
+++ runtests.py
@@ -4,7 +4,7 @@ import doctest
 
 
 files = ("stereotools.txt",)
-opts = doctest.REPORT_ONLY_FIRST_FAILURE|doctest.ELLIPSIS
+opts = doctest.REPORT_ONLY_FIRST_FAILURE|doctest.ELLIPSIS|doctest.NORMALIZE_WHITESPACE
 
 for f in files:
     doctest.testfile(f, optionflags=opts)
--- stereotools.py
+++ stereotools.py
@@ -17,7 +17,7 @@ def normalise(img):
     return normalised
 
 
-def get_vert_edges(img, cuts=20):
+def get_vert_edges(img, cuts=20, stepsize = 5):
     """ Returns a list (len cuts) of lists of distinct vertical edges in
         the image.
 
@@ -30,12 +30,17 @@ def get_vert_edges(img, cuts=20):
     for row in range(start, end, step):
         rowedges = []
         lastval = None
-        for col in range(img.width):
+        for col in range(0, img.width, stepsize):
             val = img[row, col]
-            if lastval is not None and abs(val - lastval) > 70:
-                rowedges.append(col)
+            if lastval is not None and abs(val - lastval) > 100:
+                rowedges.append(col - (stepsize / 2))
             lastval = val
         if len(rowedges) >= 2:
             edges.append((row, rowedges))
     return edges
 
+
+def add_dot(img, row, col):
+    """ Adds a black dot *in-place* on an image a row, col.
+    """
+    img[row, col] = 0
