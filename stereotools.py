ef54eee202381cd1ccda066ec91eda27861a098a
Robert Wallhead <rwallhead@gmail.com>
2012-04-23 21:04:04 +1000

    Working common edge detection
    Early days, and just one image, but the idea isn't totally broken at least :)

--- stereotools.py
+++ stereotools.py
@@ -17,13 +17,20 @@ def normalise(img):
     return normalised
 
 
+def add_dot(img, row, col, diam=5):
+    """ Adds a black dot *in-place* on an image a row, col.
+    """
+    cv.Circle(img, (col, row), diam, (0,), -1)
+    cv.Circle(img, (col, row), diam, (255,))
+
+
 def get_vert_edges(img, cuts=20, stepsize = 5):
     """ Returns a list (len cuts) of lists of distinct vertical edges in
         the image.
 
         img is a cv image from cv.LoadImage
     """
-    edges = []
+    edges = {}
     end = img.height
     step = end / cuts
     start = step / 2
@@ -36,11 +43,38 @@ def get_vert_edges(img, cuts=20, stepsize = 5):
                 rowedges.append(col - (stepsize / 2))
             lastval = val
         if len(rowedges) >= 2:
-            edges.append((row, rowedges))
+            edges[row] = rowedges
     return edges
 
 
-def add_dot(img, row, col):
-    """ Adds a black dot *in-place* on an image a row, col.
+def calc_distances(edgedata):
+    """ Returns the distance between every point and every other for a list
+        of edges found by get_vert_edges.
     """
-    img[row, col] = 0
+    results = {}
+    for row, edges in edgedata.items():
+        rowdists = {}
+        for e_from in range(len(edges)):
+            for e_to in range(e_from, len(edges)):
+                if e_from != e_to:
+                    rowdists[abs(edges[e_from] - edges[e_to])] = (edges[e_from], edges[e_to])
+        results[row] = rowdists
+    return results
+
+
+def get_common_dists(left, right, prox=5):
+    """ Compares two sets of edge distances
+    """
+    matching = {}
+    for row in left.keys():
+        if row in right.keys():
+            rowmatches = []
+            l_dists = left[row].keys()
+            r_dists = right[row].keys()
+            for ld in l_dists:
+                for rd in r_dists:
+                    if abs(ld - rd) < prox:
+                        rowmatches.append(((ld, left[row][ld]), (rd, right[row][rd])))
+            if len(rowmatches) > 0:
+                matching[row] = rowmatches
+    return matching
